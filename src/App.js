6938464d4477d1e80e53d4188665c497a4a5e037
MAMOUN-kamal-alshisani <mamoun.alshishani@yahoo.com>
2021-06-29 17:45:29 +0300

    lab3

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 import Beast from './components/Beast';
+
 class App extends React.Component{
 render(){
 
   return(
     
    <>
+
 <Header/>
     <Main/>
     <Footer/>
